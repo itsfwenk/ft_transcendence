da7b8cb12a1737d5e498b012b8d27fd925d6a6ff
Franck Li <fli@bess-f4r2s3.clusters.42paris.fr>
2025-03-05 17:04:52 +0100

    server listen 3000

--- server-side-pong/server-side-pong.ts
+++ server-side-pong/server-side-pong.ts
@@ -51,8 +51,8 @@ setInterval(() => {
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 5000 });
-    console.log(`🚀 Server running at http://localhost:5000`);
+    await fastify.listen({ port: 3000 });
+    console.log(`🚀 Server running at http://localhost:3000`);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);

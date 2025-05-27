7daaace9a8d9c931cfaa0487a2f6ea6b4fccac3b
Franck Li <fli@f6r6s9.clusters.42paris.fr>
2025-05-27 15:05:49 +0200

    button to div in custom page

--- frontend/srcs/pages/customGame.ts
+++ frontend/srcs/pages/customGame.ts
@@ -7,43 +7,46 @@ export default function customGame() {
             <div class="text-black font-jaro text-9xl mt-16 mb-16 select-none">${i18n.t('general.pongGame')}</div>
             <div class="flex flex-col justify-center items-center gap-6">
                 <div class="flex gap-4 mb-8">
-                    <button
+                    <div
                         id="defaultTheme"
-                        class="w-40 h-12 rounded-md bg-gray-500 text-white font-jaro font-semibold cursor-pointer select-none
+                        class="button w-40 h-12 rounded-md bg-gray-500 text-white font-jaro font-semibold cursor-pointer select-none
                             hover:translate-y-2
-                            hover:[box-shadow:0_10px_0_0_transparent,0_15px_0_0_transparent]
-                            hover:border-b-[1px] hover:border-transparent
+                            hover:[box-shadow:0_0px_0_0_#374151,0_0px_0_0_#37415141]
+                            hover:border-b-[0px]
                             transition-all duration-150
                             [box-shadow:0_10px_0_0_#374151,0_15px_0_0_#37415141]
                             border-b-[1px] border-gray-400"
                         data-theme-palette='{"background": "#DDDDDD", "paddle1": "#4F46E5", "paddle2": "#DC2626", "ball": "black", "line": "rgba(0, 0, 0, 0.2)", "score": "rgba(200, 200, 200, 0.7)"}'
+                        data-shadow-main="#374151"
+                        data-shadow-accent="#37415141"
+                        data-border-color="gray-400"
                     >
-                        Default
-                    </button>
+                        <span class='flex flex-col justify-center items-center h-full text-white font-jaro'>Default</span>    
+                    </div>
 
-                    <button
+                    <div
                         id="BnWTheme"
-                        class="w-40 h-12 rounded-md bg-gray-800 text-white font-jaro font-semibold cursor-pointer select-none
+                        class="button w-40 h-12 rounded-md bg-gray-800 text-white font-jaro font-semibold cursor-pointer select-none
 							hover:translate-y-2 
-							hover:[box-shadow:0_0px_0_0_#1b6ff8,0_0px_0_0_#1b70f841]
+							hover:[box-shadow:0_0px_0_0_#000000,0_0px_0_0_#00000041]
 							hover:border-b-[0px]
 							transition-all duration-150 
-							[box-shadow:0_10px_0_0_#193cb8,0_15px_0_0_#1b70f841]
+							[box-shadow:0_10px_0_0_#000000,0_15px_0_0_#00000041]
                             border-b-[1px] border-gray-400"
                         data-theme-palette='{"background": "#1A1A1A", "paddle1": "white", "paddle2": "white", "ball": "white", "line": "rgba(255, 255, 255, 0.2)", "score": "rgba(255, 255, 255, 0.6)"}'
                         data-shadow-main="#000000"
                         data-shadow-accent="#00000041"
                         data-border-color="#4D4D4D"
                     >
-                        Black n White
-                    </button>
+                        <span class='flex flex-col justify-center items-center h-full text-white font-jaro'>Black/White</span>
+                    </div>
 
-                    <button
+                    <div
                         id="NetherBtn"
-                        class="w-40 h-12 flex items-center justify-center rounded-md bg-[#8B0000] text-white font-jaro font-semibold cursor-pointer select-none
+                        class="button w-40 h-12 flex items-center justify-center rounded-md bg-[#8B0000] text-white font-jaro font-semibold cursor-pointer select-none
                             hover:translate-y-2
-                            hover:[box-shadow:0_10px_0_0_transparent,0_15px_0_0_transparent]
-                            hover:border-b-[1px] hover:border-transparent
+                            hover:[box-shadow:0_0px_0_0_#5A0000,0_0px_0_0_#5A000041]
+                            hover:border-b-[0px]
                             transition-all duration-150
                             [box-shadow:0_10px_0_0_#5A0000,0_15px_0_0_#5A000041]
                             border-b-[1px] border-[#B22222] mb-[11px]"
@@ -52,15 +55,15 @@ export default function customGame() {
                         data-shadow-accent="#5A000041"
                         data-border-color="#B22222"
                     >
-                        Nether
-                    </button>
+                        <span class='flex flex-col justify-center items-center h-full text-white font-jaro'>Nether</span>
+                    </div>
 
-                    <button
+                    <div
                         id="MarioBtn"
-                        class="w-40 h-12 flex items-center justify-center rounded-md bg-red-600 text-white font-jaro font-semibold cursor-pointer select-none
+                        class="button w-40 h-12 flex items-center justify-center rounded-md bg-red-600 text-white font-jaro font-semibold cursor-pointer select-none
                             hover:translate-y-2
-                            hover:[box-shadow:0_10px_0_0_transparent,0_15px_0_0_transparent]
-                            hover:border-b-[1px] hover:border-transparent
+                            hover:[box-shadow:0_0px_0_0_#1C3D88,0_0px_0_0_#1C3D8841]
+                            hover:border-b-[0px]
                             transition-all duration-150
                             [box-shadow:0_10px_0_0_#1C3D88,0_15px_0_0_#1C3D8841]
                             border-b-[1px] border-blue-500 mb-[11px]"
@@ -69,17 +72,25 @@ export default function customGame() {
                         data-shadow-accent="#1C3D8841"
                         data-border-color="#3B82F6"
                     >
-                        Super Mario
-                    </button>
+                        <span class='flex flex-col justify-center items-center h-full text-white font-jaro'>Super Mario</span>
+                    </div>
                 </div>
 
                 <canvas id="themePreviewCanvas" width="800" height="400" class="border-2 border-gray-300 bg-gray-100 rounded-md"></canvas>
 
-                <div id="backBtn" class='button w-24 h-13 mt-10 bg-gray-700 rounded-full cursor-pointer select-none
-                    hover:translate-y-2  hover:[box-shadow:0_0px_0_0_#1b6ff8,0_0px_0_0_#1b70f841]
-                    hover:border-b-[0px]
-                    transition-all duration-150 [box-shadow:0_10px_0_0_#181818,0_15px_0_0_#1b70f841]
-                    border-b-[1px] border-gray-400'>
+                <div 
+                    id="backBtn"
+                    class='button w-24 h-13 mt-10 bg-gray-700 rounded-full cursor-pointer select-none
+                        hover:translate-y-2 
+                        hover:[box-shadow:0_0px_0_0_#000000,0_0px_0_0_#00000041]
+                        hover:border-b-[0px]
+                        transition-all duration-150
+                        [box-shadow:0_10px_0_0_#000000,0_15px_0_0_#00000041]
+                        border-b-[1px] border-gray-400'
+                    data-shadow-main="#000000"
+                    data-shadow-accent="#00000041"
+                    data-border-color="#4D4D4D"
+                >
                     <span class='flex flex-col justify-center items-center h-full text-white font-jaro'>${i18n.t('general.back')}</span>
                 </div>
             </div>
@@ -108,7 +119,7 @@ export default function customGame() {
             const initialBallY = canvas.height / 2;
             const initialPaddleY = (canvas.height - paddleHeight) / 2;
 
-            const themeButtons = document.querySelectorAll('button[data-theme-palette]');
+            const themeButtons = document.querySelectorAll('div[data-theme-palette]');
 
             const defaultPalette: ThemePalette = {
                 background: "#DDDDDD",
@@ -162,27 +173,38 @@ export default function customGame() {
             drawCanvas(activeThemePalette);
 
             themeButtons.forEach(buttonElement => {
-                const button = buttonElement as HTMLButtonElement;
-                const themePaletteString = button.dataset.themePalette;
+                // const button = buttonElement;
+                const divButton = buttonElement as HTMLDivElement
+                const themePaletteString = divButton.dataset.themePalette;
 
                 if (themePaletteString) {
                     const themePalette: ThemePalette = JSON.parse(themePaletteString);
 
-                    button.addEventListener('mouseenter', () => {
+                    divButton.addEventListener('mouseenter', () => {
                         drawCanvas(themePalette);
                     });
 
-                    button.addEventListener('mouseleave', () => {
+                    divButton.addEventListener('mouseleave', () => {
                         drawCanvas(activeThemePalette);
                     });
 
-                    button.addEventListener('click', () => {
+                    divButton.addEventListener('click', () => {
                         activeThemePalette = themePalette;
                         drawCanvas(activeThemePalette);
 
                         localStorage.setItem('selectedGamePalette', JSON.stringify(activeThemePalette));
                         themeButtons.forEach(btn => btn.classList.remove('active-theme-button'));
-                        button.classList.add('active-theme-button');
+                        divButton.classList.add('active-theme-button');
+                    });
+
+                    divButton.addEventListener('mousedown', () => {
+                        divButton.classList.add('is-active');
+                    });
+                    divButton.addEventListener('mouseup', () => {
+                        divButton.classList.remove('is-active');
+                    });
+                    divButton.addEventListener('mouseleave', () => {
+                        divButton.classList.remove('is-active');
                     });
                 }
             });
@@ -190,32 +212,51 @@ export default function customGame() {
             const style = document.createElement('style');
             style.innerHTML = `
                 .active-theme-button {
-                    transform: translateY(2px);
-                    box-shadow: 0 10px 0 0 transparent, 0 15px 0 0 transparent !important;
-                    border-bottom: 1px solid transparent !important;
-
-                    transition: transform 150ms ease-out, box-shadow 150ms ease-out, border 150ms ease-out;
+                    transform: translateY(10px) !important;
+                    box-shadow: 0 0px 0 0 var(--shadow-main-color), 0 0px 0 0 var(--shadow-accent-color) !important;
+                    border-bottom: 0px solid transparent !important;
                     filter: brightness(0.9);
+                    transition: transform 150ms ease-out, box-shadow 150ms ease-out, border 150ms ease-out;
                 }
 
-                button:active {
-                    outline: none !important;
-                    border: 1px solid transparent !important;
-                    box-shadow: 0 10px 0 0 transparent, 0 15px 0 0 transparent !important;
-                    
+                .active-theme-button:hover {
                     transform: translateY(2px) !important;
-                    filter: brightness(0.9) !important;
+                    box-shadow: 0 0px 0 0 var(--shadow-main-color), 0 0px 0 0 var(--shadow-accent-color) !important;
+                    border-bottom: 0px solid transparent !important;
+                    filter: brightness(1.1);
+                    cursor: default;
                     transition: none;
                 }
 
-                button:focus {
-                    outline: none !important;
-                    border: none !important;
-                    box-shadow: none !important;
+                .is-active {
+                    transform: translateY(2px) !important;
+                    box-shadow: 0 0px 0 0 var(--shadow-main-color), 0 0px 0 0 var(--shadow-accent-color) !important;
+                    border-bottom: 0px solid transparent !important;
+                    filter: brightness(0.9);
+                    transition: none;
+                }
+
+                .button {
+                    position: relative;
+                    transition: all 150ms ease-out;
                 }
             `;
             document.head.appendChild(style);
 
+            function setButtonThemeVars(button: HTMLDivElement) {
+                const shadowMain = button.dataset.shadowMain || '#000000';
+                const shadowAccent = button.dataset.shadowAccent || '#00000041';
+                const borderColor = button.dataset.borderColor || '#4D4D4D';
+
+                button.style.setProperty('--shadow-main-color', shadowMain);
+                button.style.setProperty('--shadow-accent-color', shadowAccent);
+                button.style.setProperty('--border-color', borderColor);
+            }
+
+            const allButtons = document.querySelectorAll('div.button');
+            allButtons.forEach(divButton => {
+                setButtonThemeVars(divButton as HTMLDivElement);
+            });
             // const defaultButton = document.getElementById('defaultTheme') as HTMLButtonElement;
             // if (defaultButton) {
             //     defaultButton.click();
@@ -254,7 +295,7 @@ export default function customGame() {
             }
 
             if (!foundSavedTheme) {
-                const defaultButton = document.getElementById('defaultTheme') as HTMLButtonElement;
+                const defaultButton = document.getElementById('defaultTheme') as HTMLDivElement;
                 if (defaultButton) {
                     defaultButton.click();
                 }
